aac21cc5e2d682b4a700b88d3c82589dfaee9cb5
Badal Aryal <badal.aryal@gmail.com>
2025-07-10 16:11:43 +0545

    Initial Commit

---
+++ MovieSearch/MovieSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieSearchApp.swift
+//  MovieSearch
+//
+//  Created by Badal Aryal on 10/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct MovieSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
